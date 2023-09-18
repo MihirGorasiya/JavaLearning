7c6ec6b716b8591b5e9186ca567f1f0dddf90fbc
Mihir Gorasiya <mihirpatel9408@gmail.com>
2023-09-17 21:41:29 -0400

    Condition and loop

---
+++ src/Loops.java
@@ -0,0 +1,7 @@
+class loops {
+    public void forLoop(int index){
+        for(int i = 0; i< index;i++){
+            System.out.print("*");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+//        Random rand = new Random();
+//        int randomNumber = rand.nextInt(11);
+//
+//        System.out.println(randomNumber);
+//
+//        if(randomNumber > 5){
+//            System.out.println("It is greater than 5");
+//        }
+//        else {
+//            System.out.println("It is less than 5");
+//        }
+    loops l = new loops();
+    l.forLoop(3);
+
+
+    }
+}
